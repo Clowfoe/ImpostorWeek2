6b57d309cc0e0e97f597abc53cf28c4a32a4c2bd
Clowfoe <70791621+Clowfoe@users.noreply.github.com>
2021-08-26 02:37:34 -0400

    ass
    ass

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -27,6 +27,8 @@ class GameOverSubstate extends MusicBeatSubstate
 			case 'bf-pixel':
 				stageSuffix = '-pixel';
 				daBf = 'bf-pixel-dead';
+			case 'bf-fall':
+				daBf = 'bf-fall';
 			default:
 				daBf = 'bf';
 		}
