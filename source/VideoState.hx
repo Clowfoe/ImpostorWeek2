04cf67589b90b76b5225e9fe8d3b2e3ac1551ff7
Clowfoe <70791621+Clowfoe@users.noreply.github.com>
2021-08-27 15:02:43 -0400

    comment

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -45,7 +45,7 @@ class VideoState extends MusicBeatState
 		transClass = toTrans;
 		if (frameSkipLimit != -1 && GlobalVideo.isWebm)
 		{
-			GlobalVideo.getWebm().webm.SKIP_STEP_LIMIT = frameSkipLimit;	
+			//GlobalVideo.getWebm().webm.SKIP_STEP_LIMIT = frameSkipLimit;	
 		}
 	}
 	
